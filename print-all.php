4f2bb3942a77e443775a0e6a692054f90848b5d6
cieslak.dariusz <cieslak.dariusz@f5575bd4-ea59-11dd-85a8-df16bb8a7048>
2009-03-02 09:30:37 +0000

    print-all.php introduced

---
+++ print-all.php
@@ -0,0 +1,34 @@
+<?php
+
+if(!isset($db)) require("common.php");
+
+$pages = array();
+if (isset($_REQUEST["title"]) {
+    $title = $_REQUEST["title"]
+}
+else {
+    $title = "CMSWiki printAll preview"
+}
+
+clearstatcache();
+$arr = array();
+$dir = opendir($db);
+while($file = readdir($dir))
+{
+	if($file[0] == ".")
+		continue;
+	if(!strpos($file, ".html"))
+		continue;
+	$k = strrpos($file, ".");
+
+	$encodedPageName = substr($file, 0, $k);
+    $pageBody = getWikiPage($encodedPageName);
+    $page = urldecode($encodedPageName);
+
+    $pages[] = array($page, $pageBody);
+}
+
+include(PRINT_TEMPLATE);
+
+
+?>
